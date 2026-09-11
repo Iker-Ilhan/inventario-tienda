df54aa83d7e1d776c190930b501b72a03ccbf9ff
Iker-Ilhan <ikerilhan019@gmail.com>
2026-09-11 12:29:50 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 // ===== CONEXIÓN A TU BASE DE DATOS "proyecto" =====
-const MONGODB_URL = 'mongodb://localhost:27017/proyecto';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/inventario';
 
 mongoose.connect(MONGODB_URL)
   .then(() => console.log('✅ ¡Conectado a MongoDB - Base de datos: proyecto!'))
